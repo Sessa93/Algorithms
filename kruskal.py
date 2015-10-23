b1ff709ad941c2352820cf3a3944165746ce1e05
Andrea Sessa <andrea.sessa@mail.polimi.it>
2015-10-23 15:16:46 +0200

    SemiFinal version of the Kruskal's algorithm

--- kruskal.py
+++ kruskal.py
@@ -18,16 +18,40 @@ class Edge:
         self.n1 = n1
         self.n2 = n2
 
+def compareTo(e1, e2):
+    if e1.cost >= e2.cost:
+        return False
+    return True
+
 def kruskal(nodes, edges):
     n = nodes.length
-    SORTED_EDGES = sort(edges,key) #??
-    #Initialization of S, that is the container of the single connected component
-    S = range(n)
+    SORTED_EDGES = edges.sort(compareTo) #O(m*log(m))
+
+    #Initialization of S to keep track of the connected components
+    S = dict()
+    for n in nodes:
+        S.append(n,S.length)
+
     R = []
+
     while R.length < n:
         curr_edge = SORTED_EDGES.pop() #The minimum costing edge
-        
+
+        #Is the selected edge endpoints connected in two separated connected components? (ie. is it creating a cycle?)
+        if S.get(curr_edge.n1) != S.get(curr_edge.n2):
+            sub = S.get(curr_edge.n2)
+            actual = S.get(curr_edge.n1)
+            #The new edge is NOT creating a cycle
+            #So we merge the two connected components
+            for e in S:
+                if e.value == sub:
+                    e.value = actual
+            R.append(curr_edge)
         pass
+    #The while cost O(n^2)
+    #So the total complexity is O(m*logm + n^2)
+
+    return R
 
 ####
 #   MAIN
