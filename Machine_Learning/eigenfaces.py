8ed90b1d1dc7e35397b4fa251b48acc576a7e6ca
Sessa93 <andrea.sessa@mail.polimi.it>
2016-04-25 21:11:13 +0200

    Completed the randomized pca alg

--- Machine_Learning/eigenfaces.py
+++ Machine_Learning/eigenfaces.py
@@ -1,28 +1,40 @@
-from __future__ import print_function
+# Author Andrea Sessa
+# Plotting the first 10 eigenvectors extracted by Randomized PCA
 
-from time import time
 import logging
+from time import time
+
+from numpy.random import RandomState
 import matplotlib.pyplot as plt
 
-from sklearn.cross_validation import train_test_split
-from sklearn.datasets import fetch_lfw_people
-from sklearn.grid_search import GridSearchCV
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.decomposition import RandomizedPCA
-from sklearn.svm import SVC
+from sklearn.datasets import fetch_olivetti_faces
+from sklearn.cluster import MiniBatchKMeans
+from sklearn import decomposition
 
-print(__doc__)
-
-# Display progress logs on stdout
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
-
-n_row, n_col = 2, 3
-n_components = n_row * n_col
+#Display the log to the stdout
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+rows = 2
+cols = 5
+n_components = rows*cols
 image_shape = (64, 64)
-#rng = RandomState(0)
+rng = RandomState(0)
 
-def plot_gallery(title, images, n_col=n_col, n_row=n_row):
+# Load the dataset from http://cs.nyu.edu/~roweis/data/olivettifaces.mat
+dataset = fetch_olivetti_faces(shuffle=True, random_state=rng)
+faces = dataset.data
+
+n_samples, n_features = faces.shape
+
+#Subtract the mean to perform the PCA analysis
+faces_centered = faces - faces.mean(axis=0)
+
+# local centering
+faces_centered -= faces_centered.mean(axis=1).reshape(n_samples, -1)
+
+print("Dataset consists of %d faces" % n_samples)
+
+#Utility function to plot the Eigenfaces
+def plot_gallery(title, images, n_col=cols, n_row=rows):
     plt.figure(figsize=(2. * n_col, 2.26 * n_row))
     plt.suptitle(title, size=16)
     for i, comp in enumerate(images):
@@ -35,61 +47,19 @@ def plot_gallery(title, images, n_col=n_col, n_row=n_row):
         plt.yticks(())
     plt.subplots_adjust(0.01, 0.05, 0.99, 0.93, 0.04, 0.)
 
+rand_pca = decomposition.RandomizedPCA(n_components=n_components, whiten=True)
 
-###############################################################################
-# Download the data, if not already on disk and load it as numpy arrays
+# Plot a sample of the input data
+plot_gallery("First centered Olivetti faces", faces_centered[:n_components])
 
-lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
-
-# introspect the images arrays to find the shapes (for plotting)
-n_samples, h, w = lfw_people.images.shape
-
-# for machine learning we use the 2 data directly (as relative pixel
-# positions info is ignored by this model)
-X = lfw_people.data
-n_features = X.shape[1]
-
-# the label to predict is the id of the person
-y = lfw_people.target
-target_names = lfw_people.target_names
-n_classes = target_names.shape[0]
-
-print("Total dataset size:")
-print("n_samples: %d" % n_samples)
-print("n_features: %d" % n_features)
-print("n_classes: %d" % n_classes)
-
-
-###############################################################################
-# Split into a training set and a test set using a stratified k fold
-
-# split into a training and testing set
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, random_state=42)
-
-
-###############################################################################
-# Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
-# dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
-
-print("Extracting the top %d eigenfaces from %d faces"
-      % (n_components, X_train.shape[0]))
+#Perform the randomized PCA
+print("Extracting the first %d %s dimensions..." % (n_components, "RandomizedPCA"))
 t0 = time()
-pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
-train_time = time() - t0
-print("done in %0.3fs" % (train_time))
+data = faces_centered
+rand_pca.fit(data)
+train_time = (time() - t0)
+print("Done in %0.3fs" % train_time)
 
-eigenfaces = pca.components_.reshape((n_components, h, w))
-
-#Plotting the eigenvectors(eigenfaces)
-
-plot_gallery('%s - Train time %.1fs' % ("prova", train_time), pca.components_[:n_components])
-
-#print("Projecting the input data on the eigenfaces orthonormal basis")
-#t0 = time()
-#X_train_pca = pca.transform(X_train)
-#X_test_pca = pca.transform(X_test)
-#print("done in %0.3fs" % (time() - t0))
-
+components_ = rand_pca.components_
+plot_gallery('%s - Train time %.1fs' % ("RandomizedPCA", train_time), components_[:n_components])
 plt.show()
