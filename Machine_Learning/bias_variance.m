ee8cfd8dee8451a7d250c5adaaedb29090e09747
Sessa93 <andrea.sessa@mail.polimi.it>
2016-04-28 17:10:23 +0200

    Completed intial bias-variance analisys

--- Machine_Learning/bias_variance.m
+++ Machine_Learning/bias_variance.m
@@ -3,7 +3,7 @@ close all;
 clc;
 
 %% Create the dataset
-n_points = 1000;
+n_points = 10000;
 eps = 0.7;
 func = @(x)(1 +1 /2 * x +1/10 *x.^2); %The true model! 
 
@@ -42,8 +42,9 @@ xlabel('w0');
 ylabel('w1');
 zlabel('w2');
 
-%% Estimating bias-variance
+%% Trying different models
 n_rep = 100;
+
 for ii= 1:n_rep
    % Sample generation
    x = 5 * rand(n_points,1);
@@ -59,18 +60,20 @@ for ii= 1:n_rep
 end
 
 figure();
-plot3(real_par(1), real_par(2), real_par(3),'bx');
+plot3(real_par(1), real_par(2), real_par(3),'bo');
 hold on;
 grid on;
 
 plot3(best_par(1), best_par(2), best_par(3), 'ro');
-plot3(lin_coeff(1), lin_coeff(2), lin_coeff(3), 'r.');
-plot3(qua_coeff(1), qua_coeff(2), qua_coeff(3),'b.');
+plot3(lin_coeff(:,1), lin_coeff(:,2), lin_coeff(:,3), 'r.');
+plot3(qua_coeff(:,1), qua_coeff(:,2), qua_coeff(:,3),'b.');
 
 title('Parameter Space');
 xlabel('w0');
 ylabel('w1');
 zlabel('w2');
+
+%% Estimating Bias-Variance
 
 x_new = 5 * rand();
 t_new = func(x_new) + eps * randn(1,1);
@@ -78,6 +81,7 @@ t_new = func(x_new) + eps * randn(1,1);
 x_enh_new = [1 x_new 0];
 phi_new = [1 x_new x_new.^2];
 
+% Prediction for the new point
 for ii=1:n_rep
    y_pred_lin(ii) = lin_coeff(ii,:) * x_enh_new';
    y_pred_qua(ii) = qua_coeff(ii,:) * phi_new';
@@ -94,10 +98,56 @@ variance_qua = var(y_pred_qua);
 var_t_qua = error_qua - variance_qua - bias_qua;
 
 disp('---Single Point----');
-disp(['Linear Error: ' num2str(error_lin)]);
-disp(['Linear Bias: ' num2str(bias_lin)]);
-disp(['Linear Variance: ' num2str(variance_lin)]);
-disp(['Linerar sigma: ' num2str(var_t_lin)]);
+disp('Linear Model');
+disp(['Linear error: ' num2str(error_lin)]);
+disp(['Linear bias: ' num2str(bias_lin)]);
+disp(['Linear variance: ' num2str(variance_lin)]);
+disp(['Linear sigma: ' num2str(var_t_lin)]);
+disp('Quadratic Model');
+disp(['Quadratic error: ' num2str(error_qua)]);
+disp(['Quadratic bias: ' num2str(bias_qua)]);
+disp(['Quadratic variance: ' num2str(variance_qua)]);
+disp(['Quadratic sigma: ' num2str(var_t_qua)]);
+
+%% More Accurate estimation...
+n_samples = 101;
+
+x_new = linspace(0,5,n_samples)';
+t_new = func(x_new) + eps*randn(n_samples,1);
+x_enh_new = [ones(n_samples,1) x_new zeros(n_samples,1)];
+phi_new = [ones(n_samples,1) x_new x_new.^2];
+
+
+% Prediction for the new point
+for ii=1:n_rep
+   y_pred_lin_all(ii,:) = lin_coeff(ii,:) * x_enh_new';
+   y_pred_qua_all(ii,:) = qua_coeff(ii,:) * phi_new';
+end
+
+error_lin = sum(mean(repmat(t_new',n_rep,1) - y_pred_lin_all).^2)/n_samples;
+bias_lin = sum(mean(repmat(func(x_new'), n_rep,1) - y_pred_lin_all).^2) / n_samples;
+variance_lin = sum(var(y_pred_lin_all))/n_samples;
+var_t_lin = sum(error_lin - variance_lin - bias_lin)/n_samples;
+
+error_qua = sum(mean(repmat(t_new',n_rep,1) - y_pred_qua_all).^2)/n_samples;
+bias_qua = sum(mean(repmat(func(x_new'), n_rep,1) - y_pred_qua_all).^2) / n_samples;
+variance_qua = sum(var(y_pred_qua_all))/n_samples;
+var_t_qua = sum(error_lin - variance_lin - bias_lin)/n_samples;
+
+disp('---Multiple Point----');
+disp('Linear Model');
+disp(['Linear error: ' num2str(error_lin)]);
+disp(['Linear bias: ' num2str(bias_lin)]);
+disp(['Linear variance: ' num2str(variance_lin)]);
+disp(['Linear sigma: ' num2str(var_t_lin)]);
+disp('Quadratic Model');
+disp(['Quadratic error: ' num2str(error_qua)]);
+disp(['Quadratic bias: ' num2str(bias_qua)]);
+disp(['Quadratic variance: ' num2str(variance_qua)]);
+disp(['Quadratic sigma: ' num2str(var_t_qua)]);
+
+
+
 
 
 
